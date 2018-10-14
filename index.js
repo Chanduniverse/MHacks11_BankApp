91f4010e41c0200522e01f7fbe9e1fe3b07b8cb7
Sci-Phy <tristanlusardi@gmail.com>
2018-10-14 05:56:47 -0400

    Tried to set name :'( didn't work

--- index.js
+++ index.js
@@ -43,17 +43,16 @@ database.once('value', function(snapshot){
 }
 
 function pageLoad() {
-
 	var user = firebase.auth().currentUser;
-	var name, email, photoUrl, uid, emailVerified;
+	var email, photoUrl, uid, emailVerified;
 	if (user != null) {
-	  name = user.displayName;
 	  email = user.email;
 	  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
 	                   // this value to authenticate with your backend server, if
 	                   // you have one. Use User.getToken() instead.
 	}
-
+	console.log("[L. 54]EMail: " + user.email);
+	console.log("[L. 55]UID: " + user.uid);
 	changeData(name);
 }
 
@@ -74,6 +73,42 @@ function changeData(b){
     		// No user is signed in.
   		}
 	});
+}
+
+function getName() {
+	return "NOT A NAME";
+}
+
+function getTakeLoanData() {
+
+	var user = firebase.auth().currentUser;
+	var name = user.displayName;
+	console.log(name);
+
+	var date = JSON.stringify(document.getElementById("TLMM").value) + JSON.stringify(document.getElementById("TLDD").value) + JSON.stringify(document.getElementById("TLYY").value);
+	var LoanAMT = document.getElementById("TLDollars").value + "." + document.getElementById("TLCents").value;
+	var notes = document.getElementById("TLNotes").value;
+
+	// firebase.database().ref().child("USERDATA/" + name + "/" + "DateLoaned/" + ColumnNum + "/") = date;
+
+	firebase.database().ref("USERHIST/" + name + "/" + "DateLoaned/").set({
+		4: date // TODO: find how to make that a variable, and what is up with all the slashes
+	});
+	firebase.database().ref("USERHIST/" + name + "/" + "LoanAMT/").set({
+		4: LoanAMT
+	});
+	firebase.database().ref("USERHIST/" + name + "/" + "Notes/").set({
+		4: notes
+	});
+
+}
+
+function getPayBackData(name) {
+	var date = document.getElementById("PBMM").value + "/" + document.getElementById("PBDD").value + "/" + document.getElementById("PBYY").value;
+	var AmountPaid = document.getElementById("PBDollars").value + document.getElementById("PBCents").value
+	var notes = document.getElementById("PBNotes").value
+
+
 }
 
 function signout(){
@@ -134,31 +169,39 @@ function createUSER(name, mail, pass) {
 
 	var email = mail;
 	var password = pass;
-
+/*
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
 
 	  // ...
-	  console.log(errorCode);
-	  console.log(errorMessage);
+	  console.log("[L. 178]ERROR: " + errorCode);
+	  console.log("[L. 179]ERROR: " + errorMessage);
 	});
-
+*/
 	firebase.auth().onAuthStateChanged(function(user) {
+		console.log("[184] Begin auth from createUSER");
+		console.log("[185] " + user);
 		if (user) {
 			// User is signed in.
-			location.assign("transhist.html");
+			console.log("[187] User signed in");
+			var user = firebase.auth().currentUser;
+			console.log("[190] " + user.displayName);
 
+			location.assign("transhist.html");
 		} else {
 			// No user is signed in.
-			if (error.code !== null && error.message !== null) {
+		/*	if (error.code !== null && error.message !== null) {
 				console.log(error.code);
 				console.log(error.message);
-			} else {
-				console.log("error.code");
-				console.log("error.message");
-			}
+			} else {*/
+				console.log("[L. 199] error.code");
+				console.log("[L. 200] error.message");
+				var user = firebase.auth().currentUser;
+				user.displayName = name;
+				console.log("[203] " + name);
+		//	}
 		}
 	});
 };
@@ -194,32 +237,45 @@ function login() {
 }
 
 function signup() {
+
 	var name = document.getElementById("signUpName").value;
 	var email = document.getElementById("signUpEmail").value;
 	var password = document.getElementById("signUpPassword").value;
 	if (name == "" || email == "" || password == "") {
 		document.getElementById("fillout").innerHTML = "Please fill out all the fields.";
 	} else {
+		console.log("[246]Begin Signup else...");
+		console.log("[247] " + name);
+		console.log("[248] " + email);
+		console.log("[249] " + password);
 		createUSER(name, email, password);
-
+		console.log("[252] End Create user sequence");
+		firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+			  return user.updateProfile({'displayName': document.getElementById("signUpName").value});
+			}).catch(function(error) {
+			  console.log("[256]catch " + error);
+			});/*
 		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 		  // Handle Errors here.
 		  var errorCode = error.code;
 		  var errorMessage = error.message;
 		  // ...
-		  console.log(errorCode);
-		  console.log(errorMessage);
-		});
+		  console.log("[L. 248]ERROR: " + errorCode);
+		  console.log("[L. 249]ERROR: " + errorMessage);
+	  });*/
 
 		firebase.auth().onAuthStateChanged(function(user) {
+			console.log("[267]Begin auth state changed");
 			if (user) {
 		  	// User is signed in.
-			console.log("User signed in");
-			//location.assign("transhist.html");
+			console.log("[L. 255]User signed in");
+			var Leuser = firebase.auth().currentUser;
+			Leuser.displayName = name;
 			} else {
 		  	// No user is signed in.
-			console.log("ERROR: No user signed in");
+			console.log("[261]ERROR: No user signed in");
 			}
 		});
+		console.log("[279]End of signup else");
 	}
 }
